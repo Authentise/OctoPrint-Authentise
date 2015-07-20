6a507529331675ef8707a9d0db8363d10782fd6f
Nick Cloward <nick@authentise.com>
2015-07-20 16:38:08 -0600

    update plugin name

--- octoprint_authentise/__init__.py
+++ octoprint_authentise/__init__.py
@@ -17,7 +17,7 @@ class AuthentisePlugin(octoprint.plugin.TemplatePlugin):
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Authentise Plugin"
+__plugin_name__ = "Authentise"
 
 def __plugin_load__():
 	global __plugin_implementation__
