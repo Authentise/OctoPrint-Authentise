993f5cadf7b945ed69e7ed35fa11747a42c627c9
Nick Cloward <nick@authentise.com>
2015-07-24 10:30:59 -0600

    claim node on settings save
    When settings are saved it will try to claim the current node using the
    provided api token by the settings.

--- octoprint_authentise/__init__.py
+++ octoprint_authentise/__init__.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 import sys
 import os
 import subprocess
+import requests
 
 import octoprint.plugin
 
@@ -11,6 +12,7 @@ __plugin_name__ = "Authentise"
 
 # AUTHENTISE_CLIENT_PATH = 'authentise-streaming-client'
 AUTHENTISE_CLIENT_PATH = '/Applications/Authentise.app/Contents/Resources/streamus-client'
+AUTHENTISE_PRINT_API = 'https://print.dev-auth.com'
 
 class AuthentisePlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
@@ -18,11 +20,56 @@ class AuthentisePlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.AssetPlugin):
 
     def _run_client(self, *args):
+        command = (
+            AUTHENTISE_CLIENT_PATH,
+            '--logging-level',
+            'error',
+            '-c',
+            '/Applications/Authentise.app/Contents/Resources/client.conf',
+        ) + args
+
         try:
-            return subprocess.check_output((AUTHENTISE_CLIENT_PATH,) + args)
+            return subprocess.check_output(command).strip()
         except Exception as e:
             self._logger.error("Error running client command `%s` using parameters: %s", e, args)
             return
+
+    def _claim_node(self, api_key, api_secret):
+        if not self.node_uuid:
+            self._logger.error("No node uuid available to claim")
+            return False
+
+        if not api_key:
+            self._logger.error("No API Key available to claim node")
+            return False
+
+        if not api_secret:
+            self._logger.error("No API secret available to claim node")
+            return False
+
+        claim_code = self._run_client('--connection-code')
+        if claim_code:
+            self._logger.info("Got claim code: %s", claim_code)
+        else:
+            self._logger.error("Could not get a claim code from Authentise")
+            return False
+
+        url = "{}/client/claim/{}/".format(AUTHENTISE_PRINT_API, claim_code)
+        response = requests.put(url, auth=(api_key, api_secret))
+
+        if response.ok:
+            self._logger.info(response.json)
+            self._logger.info("Claimed node: %s", self.node_uuid)
+            return True
+
+        self._logger.error(
+            "Could not use claim code %s for node %s.  Response is %s, %s",
+            claim_code,
+            self.node_uuid,
+            response.status_code,
+            response.text,
+        )
+        return False
 
     def on_after_startup(self):
         self.node_version = self._run_client('--version')
@@ -31,7 +78,7 @@ class AuthentisePlugin(octoprint.plugin.StartupPlugin,
         else:
             self._logger.warning("Could not find node version")
 
-        self.node_uuid = self._run_client('--node-uuid', '--logging-level', 'error')
+        self.node_uuid = self._run_client('--node-uuid')
         if self.node_uuid:
             self._logger.info("Found node uuid: %s", self.node_uuid)
         else:
@@ -72,6 +119,10 @@ class AuthentisePlugin(octoprint.plugin.StartupPlugin,
         return [
             dict(type="settings", custom_bindings=False)
         ]
+
+    def on_settings_save(self, data):
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        self._claim_node(data['api_key'], data['api_secret'])
 
 
 def __plugin_load__():
