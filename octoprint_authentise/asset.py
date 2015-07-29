cc5fd7278a64f497b7eb8ceef2e63a7b291be735
Nick Cloward <nick@authentise.com>
2015-07-29 16:00:52 -0600

    add authentise iframe in octoprint
    AE-652
    
    Adds a tab in Octoprint named authentise that is an iframe to authentise
    model warehouse.  This also adds support for getting the node info and
    connection code using postMessage in the iframe.

--- octoprint_authentise/asset.py
+++ octoprint_authentise/asset.py
@@ -6,6 +6,9 @@ import octoprint.plugin
 class AssetPlugin(octoprint.plugin.AssetPlugin):
     def get_assets(self):
         return dict(
-            js=["js/authentise.js"],
+            js=[
+                "js/connect.js",
+                "js/models.js",
+            ],
             less=["less/authentise.less"],
         )
