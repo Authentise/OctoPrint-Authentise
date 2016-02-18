b3ec54b2a781f73af291428407b4edba63d1e29c
Scott Lemmon <scott@authentise.com>
2016-02-18 12:05:31 -0700

    Add back in the api_key connect modal.
    The saving of the api_key and secret is still not working, but getting
    it and filling it in are.
    
    AE-1446 #time 3h30m

--- octoprint_authentise/blueprint.py
+++ octoprint_authentise/blueprint.py
@@ -4,12 +4,25 @@ from __future__ import absolute_import
 
 import json
 
+import flask
 import octoprint.plugin
 
 from octoprint_authentise import helpers
 
 
 class BlueprintPlugin(octoprint.plugin.BlueprintPlugin):
+    @octoprint.plugin.BlueprintPlugin.route("/connect/", methods=["POST"])
+    def blueprint_connect(self):
+        username = flask.request.json.get('username')
+        password = flask.request.json.get('password')
+
+        status_code, response_json, cookies = helpers.login(self._settings, username, password, self._logger)
+        if not cookies:
+            return json.dumps(response_json), status_code
+
+        status_code, response_json = helpers.create_api_token(self._settings, cookies, self._logger)
+        return json.dumps(response_json), status_code
+
     @octoprint.plugin.BlueprintPlugin.route("/node/", methods=["GET"])
     def get_node(self):
         connection_code = helpers.run_client_and_wait(self._settings, args=['--connection-code'], logger=self._logger)
